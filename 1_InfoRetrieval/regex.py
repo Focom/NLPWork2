4d8f48ef6d2f20dfbff65324bbdef9be0ffbf714
Pierre Valentin <pievalentin@gmail.com>
2017-10-28 15:50:11 -0400

    All regex are now working

--- 1_InfoRetrieval/regex.py
+++ 1_InfoRetrieval/regex.py
@@ -1,14 +1,25 @@
 import re
 import codecs
 
-filename = "./factbook/geos/aa.html"
+filename = "./factbook/geos/uk.html"
 
 def findGeography(fileName):
-    pattern_all_file = re.compile("Geography - note:[\w\W]*?<div.*?\">(.*)?<\/d")
+    pattern_all_file = re.compile("(Geography - note:[\w\W]*?<div.*?\">)(.*)?(<\/d)")
     country_file = codecs.open(fileName, "r", encoding="utf8")
     test = pattern_all_file.search(country_file.read())
-    return str(test.group(1)) 
-
-findGeography(filename)
+    return str(test.group(2)) 
 
 
+def findEconomy(fileName):
+    pattern_all_file = re.compile("(Economy - overview[\w\W]*?<div class=\"category_data\">)(.*)?(<\/div)")
+    country_file = codecs.open(fileName, "r", encoding="utf8")
+    test = pattern_all_file.search(country_file.read())
+    return str(test.group(2)) 
+
+def findIntroduction(fileName):
+    pattern_all_file = re.compile("(Introduction</span>[\w\W]*?<div class=\"category_data\">)(.*)?(<\/div)")
+    country_file = codecs.open(fileName, "r", encoding="utf8")
+    test = pattern_all_file.search(country_file.read())
+    return str(test.group(2)) 
+
+print(findEconomy(filename))
