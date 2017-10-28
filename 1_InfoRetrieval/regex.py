d4c180d2937acffec329b3fd06cf860962989040
Pierre Valentin <pievalentin@gmail.com>
2017-10-28 15:14:10 -0400

    Working rergex for geography

--- 1_InfoRetrieval/regex.py
+++ 1_InfoRetrieval/regex.py
@@ -1,22 +1,14 @@
 import re
+import codecs
 
-filename = "./factbook/geos/fr.html"
+filename = "./factbook/geos/aa.html"
 
 def findGeography(fileName):
-    i = 0
-    #faire une seule string plutot que lire chaque ligne
-
     pattern_all_file = re.compile("Geography - note:[\w\W]*?<div.*?\">(.*)?<\/d")
-    
-    country_file = open(fileName, "r", encoding="utf8")
-    test = ""
-    for line in country_file:
-        i = i + 1
-        test = pattern_all_file.search(line)
-        if test:
-            good_line = line
-            break
-    print(str(test))
-    
+    country_file = codecs.open(fileName, "r", encoding="utf8")
+    test = pattern_all_file.search(country_file.read())
+    return str(test.group(1)) 
 
 findGeography(filename)
+
+
