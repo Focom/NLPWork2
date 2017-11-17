145155a7ddd5fea95e54d1ea114bf5c3f4b410af
Pierre Valentin <pievalentin@gmail.com>
2017-11-17 18:40:07 -0500

    can calculate mean now

--- 1_InfoRetrieval/elasticSearch.py
+++ 1_InfoRetrieval/elasticSearch.py
@@ -14,42 +14,71 @@ def insertAllDocuments():
         es.index(index="factbook", doc_type="country", id=i, body=content)
         i += 1
 
+
 def getNameFromQuery(query):
     data = es.search(index="factbook", doc_type="country", body=query)
     result = []
-    for i in range(0,len(data["hits"]["hits"])):
+    for i in range(0, len(data["hits"]["hits"])):
         result.append(data["hits"]["hits"][i]["_source"]["Name"])
     return result
 
+
 def askQuestion(number):
     words = helper.getWordFromQuestion(number)
+    # print(words)
     question = {
         "query": {
-                    "query_string" : {
-                        "query" : words
-                    }
+            "match": {
+                "_all": {
+                    "query": words,
+                    "operator": "or",
+                    "minimum_should_match": "79%"
                 }
+            }
+        }
     }
     return getNameFromQuery(question)
 
+
+def isDocumentRelevent(doc, expected, number):
+    for correct in expected:
+        if(doc == correct):
+            return True
+    return False
+
+
 def getPrecision(number):
+    nbCorrect = 0
+    nbPassed = 1
+    precision = []
     result = askQuestion(number)
     expected = helper.getResponse(number)
-    total_result = len(result)
-    total_expected = len(expected)
-    score = 0
-    for test in result:
-        for correct in expected:
-            if(test == correct):
-                score +=1
-    precision = score / total_result
+    for doc in result:
+        # print(doc)
+        if(isDocumentRelevent(doc, expected, number)):
+            nbCorrect += 1            
+            precision.append(nbCorrect / nbPassed)
+        nbPassed += 1
     return precision
 
-def getMAP():
-    scores = []
-    for i in range(0,20):
-        scores.append(getPrecision(i))
-    mean = helper.getAverageArray(scores)
-    return mean / 20
+def getLocalMEAN(number):
+    precisions = getPrecision(number)
+    sum = 0
+    longueur = len(precisions)
+    if(longueur == 0):
+        return 0
+    for prec in precisions:
+        sum += prec
+    result = sum/longueur
+    return result
 
-print(getMAP())
+def getMEAN():
+    precisions = []
+    for i in range (0,20):
+        precisions.append(getLocalMEAN(i))
+        print(precisions)
+    return helper.getAverageArray(precisions)
+
+
+
+print(getMEAN())
